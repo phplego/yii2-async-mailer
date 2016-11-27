b49a228a8f6dbe33e6427bb7d07c5a56ca299e62
phplego <phplego@gmail.com>
2016-11-27 17:11:54 +0300

    daemon command 2

--- src/DaemonCommand.php
+++ src/DaemonCommand.php
@@ -1,1 +1,22 @@
 <?php
+namespace PhpLego\Yii2\AsyncMailer;
+use React\EventLoop\Factory;
+use React\EventLoop\LoopInterface;
+use yii\console\Controller;
+
+abstract class DaemonCommand extends Controller
+{
+    /** @var LoopInterface */
+    protected $loop;
+    public function init()
+    {
+        parent::init();
+        $this->loop = Factory::create();
+    }
+    public function actionDaemon()
+    {
+        $this->prepare();
+        $this->loop->run();
+    }
+    abstract public function prepare();
+}
